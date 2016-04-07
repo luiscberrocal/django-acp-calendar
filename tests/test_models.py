06062584122fd1de26fdfd52ec647050b26a0e4f
LBerrocal <lberrocal@pancanal.com>
2016-04-07 07:36:06 -0500

    ACP Holidays working days count working.

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,12 +7,13 @@ test_acp-calendar
 
 Tests for `acp-calendar` models module.
 """
+import datetime
 
 from django.test import TestCase
 
 from acp_calendar import models
 from acp_calendar.initial_data import get_holiday_type_list
-from acp_calendar.models import HolidayType
+from acp_calendar.models import HolidayType, ACPHoliday
 
 
 class TestHolidayType(TestCase):
@@ -39,6 +40,44 @@ class TestACPHoliday(TestCase):
     def test_load_initial(self):
         loaded_holidays = len(get_holiday_type_list())
         self.assertEqual(11, loaded_holidays)
+
+    def test_days_in_range_generator(self):
+        start_date = datetime.date(2016, 1,1)
+        end_date = datetime.date(2016,1,31)
+        jan_days = list(ACPHoliday.days_in_range_generator(start_date, end_date))
+        self.assertEqual(31, len(jan_days))
+        self.assertEqual(jan_days[0], start_date)
+        self.assertEqual(jan_days[30], end_date)
+
+    def test_get_working_days(self):
+        start_date = datetime.date(2016, 1,1)
+        end_date = datetime.date(2016,1,31)
+        working_days = ACPHoliday.get_working_days(start_date, end_date)
+        self.assertEqual(19, working_days)
+
+    def test_get_working_days_no_work(self):
+        start_date = datetime.date(2016, 1,1)
+        end_date = datetime.date(2016,1,2)
+        working_days = ACPHoliday.get_working_days(start_date, end_date)
+        self.assertEqual(0, working_days)
+
+    def test_validate_dates_last_holiday(self):
+        first_holiday = ACPHoliday.objects.all().first()
+        last_holiday = ACPHoliday.objects.all().last()
+        try:
+            ACPHoliday.validate_dates(first_holiday.date, last_holiday.date + datetime.timedelta(days=1))
+            self.fail('Value error should have been raised')
+        except ValueError as e:
+            self.assertEqual('End date exceed the last registered holiday', str(e))
+
+    def test_validate_dates_first_holiday(self):
+        first_holiday = ACPHoliday.objects.all().first()
+        last_holiday = ACPHoliday.objects.all().last()
+        try:
+            ACPHoliday.validate_dates(first_holiday.date - datetime.timedelta(days=1), last_holiday.date)
+            self.fail('Value error should have been raised')
+        except ValueError as e:
+            self.assertEqual('Start date precedes the first registered holiday', str(e))
 
 
     def tearDown(self):
