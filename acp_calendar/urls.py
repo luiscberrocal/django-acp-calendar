e343532b21b12b04ac5969506c3374c0b76355c2
LBerrocal <lberrocal@pancanal.com>
2016-09-23 16:19:08 -0500

    Added calculator form

--- acp_calendar/urls.py
+++ acp_calendar/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url, include
-from . import views
+from django.views.generic import TemplateView
+
+from .views import CalculatorView
+
 
 urlpatterns = [
+    url(r'^calculator/',  CalculatorView.as_view(), name='calculator'),
     url(r'^api/', include('acp_calendar.api.urls', namespace='calendar-api')),
     ]
--- acp_calendar/views.py
+++ acp_calendar/views.py
@@ -1,1 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import View
 
+from .models import ACPHoliday
+from .forms import CalculatorForm
+
+
+class CalculatorView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = CalculatorForm()
+        return render(request, 'acp_calendar/calculator.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        calculator_form =  CalculatorForm(request.POST)
+        if calculator_form.is_valid():
+            start_date = calculator_form.cleaned_data['start_date']
+            end_date = calculator_form.cleaned_data['end_date']
+            working_days = ACPHoliday.get_working_days(start_date, end_date)
+            return render(request, 'acp_calendar/calculator.html', {'form': calculator_form, 'working_days': working_days})
