fb577edeacdd5145aad9cf41a0903f275afa3706
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2016-09-24 10:56:12 -0500

    Added bootstrap to calculator.html

--- acp_calendar/management/commands/acp_holidays.py
+++ acp_calendar/management/commands/acp_holidays.py
@@ -7,23 +7,46 @@ from ...initial_data import HOLIDAYS_INITIAL_DATA
 
 
 class Command(BaseCommand):
+    '''
+    $ python manage.py acp_holidays --list-initial
+
+    You will get a print out like this. The [*] on the first column means that the initial data
+    is already on the database.  The [-] on the first column means that the initial data
+    is not on the database.
+
+    Year 2006  (11 holidays)
+    ----------------------------------------------------------------------
+	[*] año_nuevo                      2006-01-01
+	[-] mártires                       2006-01-09
+	[*] martes_carnaval                2006-02-28
+	[*] viernes_santo                  2006-04-14
+	[*] día_del_trabajo                2006-05-01
+
+    ...
+
+    ======================================================================
+    Found 133 in initials
+    Found 132 in database
+
+
+    '''
     def add_arguments(self, parser):
-        parser.add_argument('optional-argument', nargs='?')
+        #parser.add_argument('optional-argument', nargs='?')
         parser.add_argument('--list-initial',
                             action='store_true',
                             dest='list_initial',
                             default=None,
                             help='List initial data')
-        parser.add_argument('--variable',
-                            action='store',
-                            dest='variable_name',
-                            default=None,
-                            help='Useful info')
-        parser.add_argument('--appended-argument',
-                            action='append',
-                            dest='appended_arg',
-                            default=None,
-                            help='Useful info')
+        # parser.add_argument('--variable',
+        #                     action='store',
+        #                     dest='variable_name',
+        #                     default=None,
+        #                     help='Useful info')
+        # parser.add_argument('--appended-argument',
+        #                     action='append',
+        #                     dest='appended_arg',
+        #                     default=None,
+        #                     help='Useful info')
 
     def handle(self, *args, **options):
         if options['list_initial']:
