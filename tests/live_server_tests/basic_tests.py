dce82083d1780be955dcf901ec9cf3c825cb16fc
LBerrocal <LBerrocal@pancanal.com>
2017-10-21 09:45:49 -0500

    Made some changes to make http server tests work. Still not working properly.

--- tests/live_server_tests/basic_tests.py
+++ tests/live_server_tests/basic_tests.py
@@ -1,48 +1,84 @@
 from django.contrib.auth.models import User
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.webdriver import WebDriver
 
-class AdminTests(StaticLiveServerTestCase):
+
+class CalculatorTests(StaticLiveServerTestCase):
 
 
     @classmethod
     def setUpClass(cls):
-        super(AdminTests, cls).setUpClass()
+        super(CalculatorTests, cls).setUpClass()
         User.objects.create_superuser(username='admin',
                                       password='pw',
                                       email='info@lincolnloop.com')
-        cls.selenium = WebDriver(executable_path= '/usr/local/bin/geckodriver')
-        cls.selenium.implicitly_wait(10)
+
 
     @classmethod
     def tearDownClass(cls):
-        cls.selenium.quit()
-        super(AdminTests, cls).tearDownClass()
 
-    def test_login_wrong_password(self):
+        super(CalculatorTests, cls).tearDownClass()
+
+    def setUp(self):
+        self.selenium = WebDriver(executable_path='/usr/local/bin/geckodriver')
+        self.selenium.implicitly_wait(10)
+        print('<<<< SETUP')
+
+    def tearDown(self):
+        self.selenium.quit()
+        print('<<<< TD')
+
+    def test_calculator(self):
         """
         Django Admin login test
         """
         # Open the admin index page
-        self.selenium.get(self.live_server_url + reverse('admin:index'))
+        self.selenium.get(self.live_server_url + reverse('calendar:calculator'))
         # Selenium knows it has to wait for page loads (except for AJAX requests)
         # so we don't need to do anything about that, and can just
         # call find_css. Since we can chain methods, we can
         # call the built-in send_keys method right away to change the
         # value of the field
-        self.selenium.find_element_by_xpath('//*[@id="id_username"]').send_keys("admin")
+        self.selenium.find_element_by_xpath('//*[@id="id_start_date"]').send_keys(4 * Keys.BACKSPACE)
+        self.selenium.find_element_by_xpath('//*[@id="id_start_date"]').send_keys("2017-10-01")
         # for the password, we can now just call find_css since we know the page
         # has been rendered
-        self.selenium.find_element_by_xpath('//*[@id="id_password"]').send_keys('pw2')
+        self.selenium.find_element_by_xpath('//*[@id="id_end_date"]').send_keys(4 * Keys.BACKSPACE)
+        self.selenium.find_element_by_xpath('//*[@id="id_end_date"]').send_keys('2017-10-31')
         # You're not limited to CSS selectors only, check
         # http://seleniumhq.org/docs/03_webdriver.html for
         # a more compreehensive documentation.
-        self.selenium.find_element_by_xpath('//*[@id="login-form"]/div[3]/input').click()
-        # Again, after submiting the form, we'll use the find_css helper
-        # method and pass as a CSS selector, an id that will only exist
-        # on the index page and not the login page
-        element = self.selenium.find_element_by_xpath('//*[@id="content"]/p')
-        message = 'Please enter the correct username and password for a staff account. ' \
-                  'Note that both fields may be case-sensitive.'
+        self.selenium.find_element_by_xpath('/html/body/div[2]/form/input[2]').click()
+
+        element = self.selenium.find_element_by_xpath('/html/body/div[2]/h3')  # /html/body/div[2]/form/input[2]
+        message = '22 working days'
+        self.assertEqual(element.text, message)
+
+    def test_calculator_error(self):
+        """
+       html body div.container div.alert.alert-error
+        """
+        # Open the admin index page
+        self.selenium.get(self.live_server_url + reverse('calendar:calculator'))
+        # Selenium knows it has to wait for page loads (except for AJAX requests)
+        # so we don't need to do anything about that, and can just
+        # call find_css. Since we can chain methods, we can
+        # call the built-in send_keys method right away to change the
+        # value of the field
+        self.selenium.find_element_by_xpath('//*[@id="id_start_date"]').send_keys(4 * Keys.BACKSPACE)
+        self.selenium.find_element_by_xpath('//*[@id="id_start_date"]').send_keys("2017-10-01")
+        # for the password, we can now just call find_css since we know the page
+        # has been rendered
+        self.selenium.find_element_by_xpath('//*[@id="id_end_date"]').send_keys(4 * Keys.BACKSPACE)
+        self.selenium.find_element_by_xpath('//*[@id="id_end_date"]').send_keys('2018-10-31')
+        # You're not limited to CSS selectors only, check
+        # http://seleniumhq.org/docs/03_webdriver.html for
+        # a more compreehensive documentation.
+        self.selenium.find_element_by_xpath('/html/body/div[2]/form/input[2]').click()
+        self.selenium.implicitly_wait(5)
+        element = self.selenium.find_elements(By.CLASS_NAME, "alert")[0]
+        message = 'End date exceed the last registered holiday'
         self.assertEqual(element.text, message)
